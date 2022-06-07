0e65117d9cbdb89c43f30a6a8705e53032776d27
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-06-07 19:08:26 +0200

    fetch info from backend and add layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import bot from "./images/leopold.png";
+import refresh from "./images/refresh.png";
+import sendButton from "./images/send-button.png";
+import React, {useState, useEffect } from 'react';
 
 function App() {
+  const [initialData, setInitialData] = useState([{}])
+
+  function refreshPage() {
+    window.location.reload(false);
+  } 
+
+  useEffect(() => {
+        fetch('/api').then(response => response.json()
+        ).then(data => setInitialData(data))
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> test </p>
-      </header>
+    <div>
+      <div className="card-container">
+        <header>
+          <p className='chat-name'>Leopold</p><br/>
+          <p className='status'>Active now</p>
+        <img className="refresh" onClick={refreshPage} src={refresh} alt="Refresh" />
+        </header>
+        <div className="card1">
+            hello
+        </div>
+        <div className="card2">
+            hi
+        </div>
+        <div className="card1">
+            how are you?
+        </div>
+        <div className="card2">
+            {initialData.mood}
+        </div>
+        <form action="/input">
+          <input type="text" className='input' placeholder='say something'/>
+          <button type="submit"> <img className="send-button" src={sendButton} alt="Send" /></button>
+        </form>
+      </div>
+      <img className="bot" src={bot} alt="Robot" />
     </div>
   );
 }
